5d1f61b40315645dddbf967dc048957f48767ec5
chrisorwa <chrisorwa@gmail.com>
2015-05-06 18:20:29 +0300

    Create text-process.R

---
+++ text-process.R
@@ -0,0 +1,33 @@
+
+#load required libraries
+library(tm)
+
+#create a term-document matrix
+CreateTdm <- function(text.data, stopwords='english', 
+                      removePunctuation=TRUE, tolower=TRUE) {
+  # create corpus
+  corpus <-Corpus(VectorSource(text.data))
+  
+  # create term document matrix 
+  tdm <-TermDocumentMatrix(corpus, 
+                           control=list(removePunctuation=removePunctuation, 
+                                        stopwords=stopwords('SMART'), tolower=tolower))
+  
+  return(tdm)
+}
+
+#calculate lower correlation limit for association mining
+GetLowerCorrLimit <- function(text.data, factor=1.0) {
+  # compute optimal lower correlation  limit
+  tokens = MC_tokenizer(text.data)
+  tokens.unique.len = length(unique(tokens))
+  corr.limit = tokens.unique.len / length(tokens) * factor
+  
+  return(corr.limit)
+}
+
+#remove hyper-link from text
+RemoveLink <-function(text){
+  text = gsub("http.*", "", text)
+  return(text)
+}
