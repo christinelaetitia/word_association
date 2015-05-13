6e909f7ae3b86826b8e20592297d18063fcecff2
chrisorwa <chrisorwa@gmail.com>
2015-05-13 15:25:34 +0300

    Update ushahidi_network_analysis.R

--- ushahidi_network_analysis.R
+++ ushahidi_network_analysis.R
@@ -5,7 +5,6 @@ source('UshaR.R')
 
 #load required libraries
 library(igraph)
-library(arules)
 library(tm)
 
 #get data
@@ -13,60 +12,33 @@ mapurl = 'https://bindup.crowdmap.com/'
 data = get_all_incidents(mapurl)
 incidents = data$incidentdescription
 
+# create corpus
+corpus = Corpus(VectorSource(incidents))
 
-#create term-document matrix
-tdm <-CreateTdm(incidents)
+# term-document matrix
+tdm2 = TermDocumentMatrix(corpus)
 
-a = MC_tokenizer(incidents)
-low_freq <- round(length(a)/length(unique(a)))*2
+# convert tdm to matrix
+m2 = as.matrix(tdm2)
 
-#find nost frequent terms
-f = findFreqTerms(x=tdm,lowfreq=low_freq)
+# word counts
+wc = rowSums(m2)
 
-# find associations for keyword
-factor_number = 10000-nrow(data)
-limit <- GetLowerCorrLimit(text.data=tdm,factor=factor_number)
-s <- findAssocs(tdm,f[1],limit)
+# get those words above the 3rd quantile
+lim = quantile(wc, probs=0.5)
+good = m2[wc > lim,]
 
-assoc_words = c()
-for (i in 1:10){
-  s <- findAssocs(tdm,f[i],limit)
-  assoc_words = rbind(assoc_words,rownames(s)[1:10])
-}
+# remove columns (docs) with zeroes
+good = good[,colSums(good)!=0]
 
-#transpose
-words = t(assoc_words)
-r = t(f[1:10])
-w = as.data.frame(rbind(words,f[1:10]))
-colnames(w) = f[1:10]
-w$asterick = c(r,'asterick')
+# adjacency matrix
+M2 = good %*% t(good)
 
-#extract word tokens from data frame
-tokens = c()
-for(j in 1:11){
-  tok = MC_tokenizer(w[,j])
-  tokens = c(tokens,tok)
-}
+# set zeroes in diagonal
+diag(M2) = 0
 
-#get unique words tokens
-unique_tokens <-unique(tokens)
-
-#create an adjacency matrix
-mat <-matrix(nrow=length(unique_tokens),ncol=length(unique_tokens))
-colnames(mat) <-unique_tokens
-rownames(mat) <-unique_tokens
-
-#find relation of top 10 words 
-for(k in 1:ncol(mat)){
-  for (l in 1:nrow(mat)){
-    g <-grep(colnames(mat)[k],colnames(w))
-    while(length(g !=0)){
-        mat[k,g] = 1
-      }
-    } 
-  }
-}
-
-
-
-
+# graph
+g = graph.adjacency(M2, weighted=TRUE, mode="undirected",
+                    add.rownames=TRUE)
+# layout
+glay = layout.fruchterman.reingold(g)
